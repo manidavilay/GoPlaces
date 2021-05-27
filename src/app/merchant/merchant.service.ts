8babee949427e677c4a6fe53aad078877e259102
manida.vilay <manida.vilay@opsone.net>
2021-05-27 17:42:49 +0200

    change merchants id

--- src/app/merchant/merchant.service.ts
+++ src/app/merchant/merchant.service.ts
@@ -11,6 +11,6 @@ export class MerchantService {
 
   // Get merchant's address by id
   getAddressesData(): Observable<any> {
-    return this.http.get('http://localhost:3000/api/address/60ad76ef7d137c51f49c0749/');
+    return this.http.get('http://localhost:3000/api/address/60afa39e8355af12b7564786/');
   }
 }
